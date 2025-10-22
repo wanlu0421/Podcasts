e7f059b694c741851e68a34d66c393212e3eb8bf
Winnie(Wanlu) Wei <wwei@centrilogic.com>
2025-10-22 15:11:42 -0400

    Initial Commit

---
+++ Podcasts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Podcasts
+//
+//  Created by wanlu wei on 2025-10-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
