70a240214fc9735e5159ee2bd95f194b90c5312c
Winnie(Wanlu) Wei <wwei@centrilogic.com>
2025-10-22 22:39:39 -0400

    PodcastDetail view implementation

--- Podcasts/Views/PodcastDetail.swift
+++ Podcasts/Views/PodcastDetail.swift
@@ -11,8 +11,32 @@ struct PodcastDetail: View {
     var podcast: Podcast
     
     var body: some View {
-        Text("PodcastDetail")
-        Text(podcast.title)
+        ScrollView{
+            VStack(alignment: .center){
+                Text(podcast.title)
+                    .font(Font.largeTitle.bold())
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                Text(podcast.publisher)
+                    .font(.title2)
+                    .foregroundStyle(.secondary)
+                AsyncImage(url: URL(string: podcast.thumbnail)) {
+                    thumbnailImage in
+                    thumbnailImage
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(8)
+                } placeholder: {
+                    Color.gray
+                }
+                .frame(width: 250, height: 250)
+                .padding()
+                Text(podcast.description)
+                    .foregroundStyle(.secondary)
+                    .padding()
+                Spacer()
+            }
+        }
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
@@ -22,8 +46,10 @@ private let mockPodcast = Podcast(
     title: "Mock Podcast",
     thumbnail: "https://cdn-images-3.listennotes.com/podcasts/the-ed-mylett-show-ed-mylett-cumulus-vQDCWVsEFw2-PEUIT9RBhZD.300x300.jpg",
     publisher: "Mock Publisher",
-    description: "A moke podcast."
+    description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
 )
-#Preview {   
-    PodcastDetail(podcast: mockPodcast)
+#Preview {
+    NavigationStack{
+        PodcastDetail(podcast: mockPodcast)
+    }
 }
